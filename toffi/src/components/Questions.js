84244d4892475257f968491486001eecf340e715
kizzanaome <naomekizzan@gmail.com>
2020-03-14 16:10:25 +0300

    setup react project

---
+++ toffi/src/components/Questions.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Question extends Component{
+    render() {
+        return (
+            <div>
+                <h1>
+                    this is a question
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Question;
